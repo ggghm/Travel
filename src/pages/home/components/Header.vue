c0d65dd7c2eb005b994ddec340076c7d36e38acb
ghm123 <939004280@qq.com>
2018-05-24 21:15:07 +0800

    524

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
 <div class="header">
-  <div class="header-left">返回</div>
-  <div class="header-input">输入城市/景点/主题</div>
-  <div class="header-right">城市</div>
+  <div class="header-left">
+    <div class="iconfont back-icon">&#xe624;</div>
+  </div>
+  <div class="header-input">
+    <span class="iconfont">&#xe632;</span>
+    输入城市/景点/主题
+    </div>
+  <div class="header-right">
+    城市
+    <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
 </div>
 </template>
 <script>
@@ -11,6 +19,7 @@ export default{
 }
 </script>
 <style lang="stylus" scoped>
+@import '~styles/varibles.styl'
  .header
   display: flex
   line-height: .86rem
@@ -19,16 +28,24 @@ export default{
   .header-left
    width: .64rem
    float: left
+  .back-icon
+   text-align: center
+   font-size: .4rem
   .header-input
    flex: 1
    height: .64rem
    line-height: .64rem
    margin-top: 0.12rem
    margin-left: .2rem
+   padding-left: .2rem
    background: #fff
    border-radius: .1rem
    color: #ccc
   .header-right
    width: 1.24rem
    float: right
-   text-align: center</style>
+   text-align: center
+  .arrow-icon
+   font-size: .24rem
+   margin-left: .04rem
+  </style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,24 @@
+<template>
+ <swiper :options="swiperOption">
+    <swiper-slide>
+      <img src="http://img1.qunarzz.com/piao/fusion/1804/22/a952a8f7bb956c02.jpg_890x330_5887dc6a.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+      <img src="http://img1.qunarzz.com/piao/fusion/1804/97/097b9aa3b6815302.jpg_890x330_a74265fc.jpg"/>
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+</swiper>
+</template>
+<script>
+export default{
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {}
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
