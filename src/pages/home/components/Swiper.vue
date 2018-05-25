c50312a3f710102cb9b8be99fddd44120c79c355
ghm123 <939004280@qq.com>
2018-05-25 10:26:20 +0800

    bangbang

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,11 +1,8 @@
 <template>
 <div class="wrapper">
  <swiper :options="swiperOption">
-    <swiper-slide>
-      <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg" />
-    </swiper-slide>
-    <swiper-slide>
-      <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1805/82/62ed2855921b1302.jpg_750x200_cb7e0793.jpg" />
+    <swiper-slide  v-for="item of swiperList"  :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" />
     </swiper-slide>
     <div class="swiper-pagination"  slot="pagination"></div>
 </swiper>
@@ -17,8 +14,18 @@ export default {
   data () {
     return {
       swiperOption: {
-        pagination: '.swiper-pagination'
-      }
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
+        }, {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/82/62ed2855921b1302.jpg_750x200_cb7e0793.jpg'
+        }
+      ]
     }
   }
 }
