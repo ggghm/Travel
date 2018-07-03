6ef6b8be4f79f7afc9c16ce344660e4e8b9cc634
ghm123 <939004280@qq.com>
2018-07-03 18:01:22 +0800

    master ok

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ import 'styles/border.css'
 import 'styles/iconfont.css'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
-// import './assets/styles/iconfont.css'
-// import 'swiper/dist/css/swiper.css'
+import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
